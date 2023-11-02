a187963f865e507bfaf380224f1277a804e97862
eliiran1231 <60235287+eliiran1231@users.noreply.github.com>
2023-11-02 12:20:23 +0200

    adjust time between user creation

--- fxios.js
+++ fxios.js
@@ -231,7 +231,7 @@ export default class Fxios {
                         }
                     })
                 })
-                await new Promise(resolve => setTimeout(resolve, 500));
+                await new Promise(resolve => setTimeout(resolve, 1000));
             }
         })
     }
